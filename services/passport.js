6adf93a528f40a02749188ce17cd7d73cb151fd7
Mohit Sawhney <mohitsawhney@Mohits-MacBook-Pro.local>
2023-10-17 23:32:36 +0530

    Finished Auth Flow

---
+++ services/passport.js
@@ -0,0 +1,37 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./../config/keys");
+const mongoose = require("mongoose");
+
+const Users = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  Users.findById(id).then((user) => done(null, user));
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (authToken, refreshToken, profile, done) => {
+      Users.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new Users({
+            googleId: profile.id,
+          })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
+    }
+  )
+);
